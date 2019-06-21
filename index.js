61ef84b9454675243955b569496936c4061e14f0
Ignacio <shoik.xtazy@gmail.com>
2019-06-21 01:11:09 -0400

    no added to array if dcto is null

--- index.js
+++ index.js
@@ -9,40 +9,45 @@ const initialUrl = '/tecnologia/celulares/?sz=40&start=0';
 
 const smartPhone = fs.readFileSync('./items.json');
 const smartPhoneParis = JSON.parse(smartPhone);
+const valuesOk = [];
 
-// function scrapeUrl(url,items){
-// 	console.log('requesting ...', url)
-// 	return axios.get(url)
-// 	.then(response => {
-// 		const $ = cheerio.load(response.data);
-// 		const pageItems = $('#search-result-items li').toArray()
-// 			.map(item => {
-// 				const $item = $(item);
-// 					return {
-// 						title: $item.find('.ellipsis_text').text(),
-// 						dcto: parseInt($item.find('.discount-badge').text()),
-// 						price: $item.find('.item-price').text().replace(/\n/g, '').substring(0, 8),
-// 						link:'https://www.paris.cl/' +  $item.find('.thumb-link').attr('href')
-// 					}
-// 			})
-// 		const nextLink = $('.page-next').attr('href');
-// 		const totalItems = items.concat(pageItems);
-// 		console.log(nextLink);
-// 		return nextLink ? scrapeUrl(nextLink, totalItems) : items;
-// 	})	
-// }
+function scrapeUrl(url,items){
+	console.log('requesting ...', url)
+	return axios.get(url)
+	.then(response => {
+		const $ = cheerio.load(response.data);
+		const pageItems = $('#search-result-items li').toArray()
+			.map(item => {
+				const $item = $(item);
+				var checkDcto = $item.find('.discount-badge').text();
+					if(checkDcto){
+						valuesOk.push( {
+							title: $item.find('.ellipsis_text').text(),
+							dcto: parseInt($item.find('.discount-badge').text()),
+							price: $item.find('.item-price').text().replace(/\n/g, '').substring(0, 8),
+							link:'https://www.paris.cl/' +  $item.find('.thumb-link').attr('href')
+						})
+					}
+					
+			})
+		const nextLink = $('.page-next').attr('href');
+		const totalItems = items.concat(pageItems);
+		console.log(nextLink);
+		return nextLink ? scrapeUrl(nextLink, totalItems) : items;
+	})	
+}
 	
-// scrapeUrl(baseUrl + initialUrl,[])
-// 	.then(items => {
-// 		fs.writeFile('./items.json',JSON.stringify(items), function(err){
-// 			if(err) return console.log(err);
-// 			console.log("Items saved !");
+scrapeUrl(baseUrl + initialUrl,[])
+	.then(items => {
+		fs.writeFile('./items.json',JSON.stringify(items), function(err){
+			if(err) return console.log(err);
+			console.log("Items saved !");
 
-// 		})
-// 	})
+		})
+	})
 
 app.get('/',(req,res) => {
-	res.json({ "result" : smartPhoneParis});
+	res.json(valuesOk);
 });
 
 app.listen(3000, () => {
