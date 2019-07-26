6c4181c33177ea9aef4f3886e2e6b45d39c73bbb
Ignacio <shoik.xtazy@gmail.com>
2019-07-26 01:24:49 -0400

    first test ok to paris site

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const cheerio = require('cheerio');
-const request = require('request');
+const axios = require('axios');
 
-request('https://www.paris.cl/tecnologia/celulares/?sz=40&start=0', function(err,res,body){
-	if(!err && res.statusCode == 200){
-		 var $ = cheerio.load(body);
-		 //console.log($('title').text());
+axios.get('https://www.paris.cl/tecnologia/celulares/smartphones/')
+	.then(response => {
+		const $ = cheerio.load(response.data);
+		var itemsOk = [];
+		const items = $('#search-result-items li').toArray()
+			.map(item => {
+				const $item = $(item);
+				if(parseInt($item.find('.discount-badge').text()) >= 40){
+					itemsOk.push( {
+						title: $item.find('.ellipsis_text').text(),
+						dcto: parseInt($item.find('.discount-badge').text()),
+						price: $item.find('.item-price').text().replace(/\n/g, ''),
+						link:'https://www.paris.cl/' +  $item.find('.thumb-link').attr('href')
+					})
+				}
+			})
 
-		 $('h3 span').each(function(i,element){
-		 	console.log($(element).text());
-		 })
-	}
-});
+		console.log(itemsOk);
+	})
