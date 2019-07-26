f8c35fc28ecff023b2e908434c34960f54f5abae
Ignacio <shoik.xtazy@gmail.com>
2019-07-26 09:07:48 -0400

    scraping recursive scraping vertical :D

--- index.js
+++ index.js
@@ -1,42 +1,37 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
+const fs = require('fs');
 
-var itemsOk = [];
+const baseUrl = "https://www.paris.cl";
+const initialUrl = '/tecnologia/celulares/?sz=40&start=0';
 
-axios.get('https://www.paris.cl/tecnologia/celulares/smartphones/')
+function scrapeUrl(url,items){
+	console.log('requesting ...', url)
+	return axios.get(url)
 	.then(response => {
 		const $ = cheerio.load(response.data);
-		const items = $('#search-result-items li').toArray()
+		const pageItems = $('#search-result-items li').toArray()
 			.map(item => {
 				const $item = $(item);
-				if(parseInt($item.find('.discount-badge').text()) >= 57){
-					itemsOk.push( {
+					return {
 						title: $item.find('.ellipsis_text').text(),
 						dcto: parseInt($item.find('.discount-badge').text()),
-						price: $item.find('.item-price').text().replace(/\n/g, ''),
+						price: $item.find('.item-price').text().replace(/\n/g, '').substring(0, 8),
 						link:'https://www.paris.cl/' +  $item.find('.thumb-link').attr('href')
-					})
-				}
+					}
 			})
+		const nextLink = $('.page-next').attr('href');
+		const totalItems = items.concat(pageItems);
+		console.log(nextLink);
+		return nextLink ? scrapeUrl(nextLink, totalItems) : items;
+	})	
+}
+	
+scrapeUrl(baseUrl + initialUrl,[])
+	.then(items => {
+		fs.writeFile('./items.json',JSON.stringify(items), function(err){
+			if(err) return console.log(err);
+			console.log("Items saved !");
 
-	})
-
-axios.get('https://simple.ripley.cl/tecno/telefonia/smartphones').then(response => {
-	const $ = cheerio.load(response.data);
-	const items = $('.catalog-container .catalog-product-item').toArray()
-		.map(item => {
-			const $item = $(item);
-			if(parseInt($item.find('.catalog-product-details__discount-tag').text())){
-				itemsOk.push( {
-					title: $item.find('.catalog-product-details__name').text(),
-					dcto: Math.abs(parseInt($item.find('.catalog-product-details__discount-tag').text())),
-					price: '',
-					link: ''
-				})
-			}
 		})
-})
-
-setTimeout(function(){
-	console.log(itemsOk);
-},5000) 
+	})
