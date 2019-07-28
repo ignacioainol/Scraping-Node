c59aa84b789f063a7180b35a495dbdd7ca336858
Ignacio <shoik.xtazy@gmail.com>
2019-07-27 21:37:13 -0400

    show json in browser

--- index.js
+++ index.js
@@ -7,39 +7,42 @@ const app = express();
 const baseUrl = "https://www.paris.cl";
 const initialUrl = '/tecnologia/celulares/?sz=40&start=0';
 
-function scrapeUrl(url,items){
-	console.log('requesting ...', url)
-	return axios.get(url)
-	.then(response => {
-		const $ = cheerio.load(response.data);
-		const pageItems = $('#search-result-items li').toArray()
-			.map(item => {
-				const $item = $(item);
-					return {
-						title: $item.find('.ellipsis_text').text(),
-						dcto: parseInt($item.find('.discount-badge').text()),
-						price: $item.find('.item-price').text().replace(/\n/g, '').substring(0, 8),
-						link:'https://www.paris.cl/' +  $item.find('.thumb-link').attr('href')
-					}
-			})
-		const nextLink = $('.page-next').attr('href');
-		const totalItems = items.concat(pageItems);
-		console.log(nextLink);
-		return nextLink ? scrapeUrl(nextLink, totalItems) : items;
-	})	
-}
-	
-scrapeUrl(baseUrl + initialUrl,[])
-	.then(items => {
-		fs.writeFile('./items.json',JSON.stringify(items), function(err){
-			if(err) return console.log(err);
-			console.log("Items saved !");
+const smartPhone = fs.readFileSync('./items.json');
+const smartPhoneParis = JSON.parse(smartPhone);
 
-		})
-	})
+// function scrapeUrl(url,items){
+// 	console.log('requesting ...', url)
+// 	return axios.get(url)
+// 	.then(response => {
+// 		const $ = cheerio.load(response.data);
+// 		const pageItems = $('#search-result-items li').toArray()
+// 			.map(item => {
+// 				const $item = $(item);
+// 					return {
+// 						title: $item.find('.ellipsis_text').text(),
+// 						dcto: parseInt($item.find('.discount-badge').text()),
+// 						price: $item.find('.item-price').text().replace(/\n/g, '').substring(0, 8),
+// 						link:'https://www.paris.cl/' +  $item.find('.thumb-link').attr('href')
+// 					}
+// 			})
+// 		const nextLink = $('.page-next').attr('href');
+// 		const totalItems = items.concat(pageItems);
+// 		console.log(nextLink);
+// 		return nextLink ? scrapeUrl(nextLink, totalItems) : items;
+// 	})	
+// }
+	
+// scrapeUrl(baseUrl + initialUrl,[])
+// 	.then(items => {
+// 		fs.writeFile('./items.json',JSON.stringify(items), function(err){
+// 			if(err) return console.log(err);
+// 			console.log("Items saved !");
+
+// 		})
+// 	})
 
 app.get('/',(req,res) => {
-	res.send('pate juan');
+	res.json({ "result" : smartPhoneParis});
 });
 
 app.listen(3000, () => {
